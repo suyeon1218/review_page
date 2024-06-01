ff5862545a1dd4df5f8d3235516e7df2e2cc55ea
Suyeon <msjgr2801@gmail.com>
2024-06-01 16:19:31 +0900

    feat: 정렬, 카테고리, 뷰타입 상수로 분리하기

---
+++ src/constants/index.ts
@@ -0,0 +1,28 @@
+export const DATE_SORT = Object.freeze({
+  DATE_ASCENDING: '날짜 오름차순',
+  DATE_ESCENDING: '날짜 내림차순',
+});
+
+export const MOVIE_CATEGORY = Object.freeze({
+  ROMANCE: '로맨스',
+  COMIC: '코믹',
+  HORROR: '공포',
+  ACTION: '액션',
+  ANIMATION: '애니메이션',
+  SF: 'SF',
+  THRILLER: '스릴러',
+  SPORT: '스포츠',
+  FANTASY: '판타지',
+  MUSIC: '음악',
+});
+
+export const VIEW_TYPE = Object.freeze({
+  LIST: '리스트 뷰',
+  CARD: '카드 뷰',
+});
+
+export const MENU_OPTIONS = Object.freeze({
+  DATE_SORT,
+  CATEGORY_FILTER: MOVIE_CATEGORY,
+  VIEW_TYPE,
+});
