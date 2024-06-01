ed702410fa03e8f8b5b018744b276409fd238906
Suyeon <msjgr2801@gmail.com>
2024-06-01 16:18:57 +0900

    chore: 필터, 정렬, 보기 방식 타입 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,27 @@
-export type ViewType = 'list' | 'card';
+import {
+  MOVIE_CATEGORY,
+  DATE_SORT,
+  MENU_OPTIONS,
+  VIEW_TYPE,
+} from './../constants/index';
+
+export type Menu = keyof typeof MENU_OPTIONS;
+export type MovieCategory = keyof typeof MOVIE_CATEGORY;
+export type Sort = keyof typeof DATE_SORT;
+export type Filter = keyof typeof MOVIE_CATEGORY;
+export type View = keyof typeof VIEW_TYPE;
+
+export interface Post {
+  id: string;
+  title: string;
+  content: string;
+  category: MovieCategory[];
+  date: Date;
+  comment: Comment[];
+}
+
+export interface Comment {
+  id: string;
+  content: string;
+  date: Date;
+}
