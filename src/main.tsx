b4ed399bc4164121e2fbc33a00eab15fb921f155
Suyeon <msjgr2801@gmail.com>
2024-06-01 14:26:53 +0900

    feat: Chakra Provider 로 Theme 전달하기

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { theme } from './styles/theme';
+import '~/styles/main.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </StrictMode>
 );
