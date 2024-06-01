88af2afc708942288c9e56e30ff64f3c2944638f
Suyeon <msjgr2801@gmail.com>
2024-06-01 13:40:06 +0900

    feat: router 기본 경로 설정하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { ListPage, DetailPage } from './pages';
+
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path='/'
+          element={<ListPage />}></Route>
+        <Route
+          path='/posts/:id'
+          element={<DetailPage />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
