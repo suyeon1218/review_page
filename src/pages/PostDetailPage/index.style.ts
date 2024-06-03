527b15fd4da7d007eb05a6e2078bdfdfeca2465d
Suyeon <msjgr2801@gmail.com>
2024-06-03 11:58:23 +0900

    feat: PostDetailPage 에서 댓글 렌더링하기

--- src/pages/PostDetailPage/index.style.ts
+++ src/pages/PostDetailPage/index.style.ts
@@ -4,11 +4,17 @@ export const Container = styled.div`
   width: 100%;
 `;
 
-export const Post = styled.div`
+export const Content = styled.div`
   width: 70%;
   margin: 0 auto;
 
   @media screen and (max-width: 768px) {
     width: 90%;
   }
+`;
+
+export const Post = styled(Content)``;
+
+export const Comment = styled(Content)`
+  margin: 20px auto;
 `;
--- src/pages/PostDetailPage/index.tsx
+++ src/pages/PostDetailPage/index.tsx
@@ -1,10 +1,35 @@
+import { useParams } from 'react-router-dom';
+import { initialPost } from '~/constants';
+import { postAPI } from '~/service';
+import * as S from './index.style';
+import CommentInput from '~/components/CommentInput';
+import CommentItem from '~/components/CommentItem';
 import GoBack from '~/components/GoBack';
+import PostBody from '~/components/PostBody';
+import PostHeader from '~/components/PostHeader';
 
 const PostDetailPage = () => {
+  const { id } = useParams();
+  const { data: post } = postAPI.useGetPostById(id as string);
+  const { comments } = post ? post : initialPost;
+
   return (
-    <div>
+    <S.Container>
       <GoBack />
-    </div>
+      <S.Post>
+        <PostHeader />
+        <PostBody />
+      </S.Post>
+      <S.Comment>
+        <CommentInput />
+        {comments.map((comment) => (
+          <CommentItem
+            key={comment.id}
+            comment={comment}
+          />
+        ))}
+      </S.Comment>
+    </S.Container>
   );
 };
 
