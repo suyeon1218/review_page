8ebb6cb044a0708b9118a4a90212e499a4723b24
Suyeon <msjgr2801@gmail.com>
2024-06-03 14:28:17 +0900

    feat: 스크랩 생성, 삭제 API 코드 구현

--- src/service/scrapAPI.ts
+++ src/service/scrapAPI.ts
@@ -1,13 +1,33 @@
-import { useQuery } from '@tanstack/react-query';
-import { Like } from '~/types';
-import { GET } from '~/api/request';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { Scrap } from '~/types';
+import { DELETE, GET, POST } from '~/api/request';
 
 const scrapAPI = {
-  useGETScrapByUserId: (userId: string) => {
+  useGetScrapByUserId: (userId: string) => {
     return useQuery({
-      queryKey: ['like'],
+      queryKey: [`scrap`],
       queryFn: async () => {
-        const response = await GET<Like[]>(`/likes/?userId=${userId}`);
+        const response = await GET<Scrap[]>(`/scrap/?userId=${userId}`);
+
+        return response;
+      },
+    });
+  },
+  useCreateScrap: (data: Omit<Scrap, 'id'>) => {
+    return useMutation({
+      mutationKey: [`deleteScrap`],
+      mutationFn: async () => {
+        const response = await POST(`/scrap`, data);
+
+        return response;
+      },
+    });
+  },
+  useDeleteScrapById: (scrapId: string | number) => {
+    return useMutation({
+      mutationKey: [`deleteScrap`],
+      mutationFn: async () => {
+        const response = await DELETE(`/scrap/${scrapId}`);
 
         return response;
       },
