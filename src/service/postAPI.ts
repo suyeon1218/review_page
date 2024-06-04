b33efe69885136ee7d9cae9e992ebf25564a296e
Suyeon <msjgr2801@gmail.com>
2024-06-04 16:48:25 +0900

    feat: 발행/삭제 뒤 토스트 UI 띄우기

--- src/service/postAPI.ts
+++ src/service/postAPI.ts
@@ -1,3 +1,4 @@
+import { useToast } from '@chakra-ui/react';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import { queryClient } from '~/api';
@@ -27,6 +28,8 @@ const postAPI = {
   },
   useCreatePost: () => {
     const navigate = useNavigate();
+    const toast = useToast();
+
     return useMutation({
       mutationKey: ['createPost'],
       mutationFn: async ({ post }: { post: Omit<Post, 'id'> }) => {
@@ -37,12 +40,20 @@ const postAPI = {
       onSuccess: (data) => {
         if (data) {
           navigate(`/posts/${data.id}`);
+          toast({
+            description: '포스트를 정상적으로 발행했어요!',
+            status: 'success',
+            duration: 5000,
+            position: 'top',
+            isClosable: true,
+          });
         }
       },
     });
   },
   useDeletePost: () => {
     const navigate = useNavigate();
+    const toast = useToast();
 
     return useMutation({
       mutationKey: ['deletePost'],
@@ -55,13 +66,20 @@ const postAPI = {
         queryClient.invalidateQueries({
           queryKey: ['allPost', `post_${postId}`],
         });
-        alert('리뷰를 삭제했어요!');
+        toast({
+          description: '포스트를 정상적으로 삭제했어요!',
+          status: 'success',
+          duration: 5000,
+          position: 'top',
+          isClosable: true,
+        });
         navigate('/');
       },
     });
   },
   useEditPost: () => {
     const navigate = useNavigate();
+    const toast = useToast();
 
     return useMutation({
       mutationKey: ['editPost'],
@@ -78,6 +96,13 @@ const postAPI = {
       },
       onSuccess: (_, { postId }) => {
         navigate(`/posts/${postId}`);
+        toast({
+          description: '포스트를 정상적으로 발행했어요!',
+          status: 'success',
+          duration: 5000,
+          position: 'top',
+          isClosable: true,
+        });
       },
     });
   },
