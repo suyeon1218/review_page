a2df922ea7a52f54ab5d7b95d65f35fabec776a4
Suyeon <msjgr2801@gmail.com>
2024-06-04 14:49:25 +0900

    feat: Rating 컴포넌트 state 기반으로 관리하기

--- src/components/Rating/index.tsx
+++ src/components/Rating/index.tsx
@@ -1,18 +1,45 @@
 import { StarIcon } from '@chakra-ui/icons';
+import { MouseEvent, useState } from 'react';
 import * as S from './index.style';
 
 interface RatingProps {
-  value: 0 | 1 | 2 | 3 | 4 | 5;
+  value?: 0 | 1 | 2 | 3 | 4 | 5;
+  readonly?: boolean;
+  onChange?: () => void;
 }
 
-const Rating = ({ value }: RatingProps) => {
+const Rating = ({ value = 0, readonly = true }: RatingProps) => {
+  const [rating, setRating] = useState<number>(value);
+
+  const handleClickIcon = (event: MouseEvent<HTMLDivElement>) => {
+    if (readonly) return;
+    if (event.target instanceof Element) {
+      const $span = event.target.closest('span') as HTMLSpanElement;
+      const { rating } = $span.dataset;
+
+      if (rating) {
+        setRating(Number(rating));
+      }
+    }
+  };
+
   return (
-    <S.Container>
-      <StarIcon color={value > 0 ? 'yellow.300' : 'gray'} />
-      <StarIcon color={value > 1 ? 'yellow.300' : 'gray'} />
-      <StarIcon color={value > 2 ? 'yellow.300' : 'gray'} />
-      <StarIcon color={value > 3 ? 'yellow.300' : 'gray'} />
-      <StarIcon color={value > 4 ? 'yellow.300' : 'gray'} />
+    <S.Container onClick={handleClickIcon}>
+      <span data-rating={1}>
+        <StarIcon color={rating > 0 ? 'yellow.300' : 'gray'} />
+      </span>
+      <span data-rating={2}>
+        <StarIcon color={rating > 1 ? 'yellow.300' : 'gray'} />
+      </span>
+      <span data-rating={3}>
+        <StarIcon color={rating > 2 ? 'yellow.300' : 'gray'} />
+      </span>
+      <span data-rating={4}>
+        <StarIcon color={rating > 3 ? 'yellow.300' : 'gray'} />
+      </span>
+      <span data-rating={5}>
+        <StarIcon color={rating > 4 ? 'yellow.300' : 'gray'} />
+      </span>
     </S.Container>
   );
 };
