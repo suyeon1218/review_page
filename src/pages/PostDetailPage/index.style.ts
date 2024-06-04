29988131695277a595c08767a3c8ccd913653825
Suyeon <msjgr2801@gmail.com>
2024-06-04 14:59:06 +0900

    style: 스크랩 아이콘 크기 키우기

--- src/pages/PostDetailPage/index.style.ts
+++ src/pages/PostDetailPage/index.style.ts
@@ -4,6 +4,14 @@ export const Container = styled.div`
   width: 100%;
 `;
 
+export const Header = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray[100]};
+  align-items: center;
+`;
+
 export const Content = styled.div`
   width: 70%;
   margin: 0 auto;
