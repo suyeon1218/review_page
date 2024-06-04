b3582d39cc1fafbf90efc63a1713bd79e88005fe
Suyeon <msjgr2801@gmail.com>
2024-06-04 14:49:49 +0900

    feat: EditorPage 에서 Input 컴포넌트 관리하기

---
+++ src/pages/EditorPage/index.style.ts
@@ -0,0 +1,100 @@
+import { Badge, Button } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+
+export const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 20px auto;
+
+  @media screen and (max-width: 768px) {
+    padding: 0 5px;
+  }
+`;
+
+export const Header = styled.header`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray[100]};
+`;
+
+export const SubmitButton = styled(Button)`
+  background-color: transparent;
+`;
+
+export const FormProperty = styled.div`
+  width: 100%;
+`;
+
+export const FormBody = styled.div`
+  width: 100%;
+  padding: 20px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray[100]};
+`;
+
+export const Col = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  padding: 0px 20px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray[100]};
+
+  @media screen and (max-width: 768px) {
+    padding: 0px 5px;
+  }
+`;
+
+export const TitleRow = styled(Col)`
+  display: flex;
+  justify-content: space-between;
+  border-bottom-color: transparent;
+`;
+
+export const Title = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+export const Scrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const CategoryRow = styled(Col)``;
+
+export const RatingRow = styled(Col)``;
+
+export const EtcRow = styled(Col)`
+  justify-content: space-between;
+`;
+
+export const styledBadge = styled(Badge)`
+  border-radius: 5px;
+`;
+
+export const LeftSideContainer = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+export const Author = styled.div`
+  font-weight: bold;
+`;
+
+export const RightSideContainer = styled.div`
+  display: flex;
+  gap: 5px;
+  color: ${({ theme }) => theme.colors.gray[500]};
+`;
+
+export const StyledButton = styled(Button)`
+  background-color: transparent;
+  padding: 0px;
+  font-weight: normal;
+  color: ${({ theme }) => theme.colors.gray[500]};
+`;
---
+++ src/pages/EditorPage/index.tsx
@@ -0,0 +1,42 @@
+import { FormProvider, useForm } from 'react-hook-form';
+import { useParams } from 'react-router-dom';
+import { postAPI } from '~/service';
+import * as S from './index.style';
+import GoBack from '~/components/GoBack';
+import InputCategory from '~/components/InputCategory';
+import InputContent from '~/components/InputContent';
+import InputRating from '~/components/InputRating';
+import InputTitle from '~/components/InputTitle';
+
+const EditorPage = () => {
+  const { id } = useParams();
+  const response = id ? postAPI.useGetPostById(id) : undefined;
+  const methods = useForm();
+
+  return (
+    <S.Form>
+      <S.Header>
+        <GoBack />
+        <S.SubmitButton>발행하기</S.SubmitButton>
+      </S.Header>
+      <FormProvider {...methods}>
+        <S.FormProperty>
+          <S.TitleRow>
+            <InputTitle defaultValue={response?.data?.title} />
+          </S.TitleRow>
+          <S.CategoryRow>
+            <InputCategory defaultValue={response?.data?.category} />
+          </S.CategoryRow>
+          <S.RatingRow>
+            <InputRating defaultValue={response?.data?.rating} />
+          </S.RatingRow>
+        </S.FormProperty>
+        <S.FormBody>
+          <InputContent defaultValue={response?.data?.content} />
+        </S.FormBody>
+      </FormProvider>
+    </S.Form>
+  );
+};
+
+export default EditorPage;
