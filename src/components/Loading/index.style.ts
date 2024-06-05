680ea139965d8b9eec4fe2b302dcb1f6fcc3e7ea
Suyeon <msjgr2801@gmail.com>
2024-06-05 15:51:19 +0900

    style: Loading 컴포넌트 높이 조정

--- src/components/Loading/index.style.ts
+++ src/components/Loading/index.style.ts
@@ -1,8 +1,9 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-  width: 100%;
+  width: 100vw;
   height: 100%;
+  min-height: 500px;
   display: flex;
   justify-content: center;
   align-items: center;
