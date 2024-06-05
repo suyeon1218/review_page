9863af0c0c3828ffb3df9aa902cf7df3099a6172
Suyeon <msjgr2801@gmail.com>
2024-06-05 16:41:26 +0900

    fix: 루트 컴포넌트에 Suspense 적용하기
    - A component suspended while responding to synchronous input. This will cause the UI to be replaced with a loading indicator. To fix, updates that suspend should be wrapped with startTransition. 에러 해결

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { StrictMode } from 'react';
+import { StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { queryClient } from './api';
 import App from './App';
 import '~/styles/main.css';
+import { Loading } from './components';
 import store from './store';
 import { theme } from './styles/theme';
 
@@ -14,7 +15,9 @@ createRoot(document.getElementById('root')!).render(
     <ChakraProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <Provider store={store}>
-          <App />
+          <Suspense fallback={<Loading />}>
+            <App />
+          </Suspense>
         </Provider>
       </QueryClientProvider>
     </ChakraProvider>
