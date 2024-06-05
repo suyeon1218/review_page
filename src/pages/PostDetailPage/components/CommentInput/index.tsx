2305e7438385c645680e968db7bfffff1a7df21d
Suyeon <msjgr2801@gmail.com>
2024-06-05 15:55:54 +0900

    feat: CommentInput 유효성 검사하기
    - 최소 1글자 최대 50글자 적용하기
    - 제출 뒤 input 초기화하기

--- src/pages/PostDetailPage/components/CommentInput/index.tsx
+++ src/pages/PostDetailPage/components/CommentInput/index.tsx
@@ -1,4 +1,5 @@
 import { MouseEvent, useRef } from 'react';
+import { useToastMessage } from '~/hooks';
 import * as S from './index.style';
 
 interface CommentInputProps {
@@ -8,18 +9,37 @@ interface CommentInputProps {
 
 const CommentInput = ({ onSubmit, defaultValue }: CommentInputProps) => {
   const inputRef = useRef<HTMLInputElement>(null);
+  const minValueErrorToast = useToastMessage(
+    '댓글은 최소 1글자 이상이어야 합니다!',
+    'error'
+  );
+  const maxValueErrorToast = useToastMessage(
+    '댓글은 최대 500글자까지 허용합니다!',
+    'error'
+  );
 
   const handleSubmitComment = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
 
     if (onSubmit && inputRef && inputRef.current) {
+      if (inputRef.current.value.length === 0) {
+        minValueErrorToast();
+        return;
+      }
+      if (inputRef.current.value.length === 0) {
+        maxValueErrorToast();
+        return;
+      }
       onSubmit(inputRef.current.value);
+      inputRef.current.value = '';
     }
   };
 
   return (
     <S.Container>
       <S.StyledInput
+        minLength={1}
+        maxLength={500}
         ref={inputRef}
         defaultValue={defaultValue}
         placeholder={'댓글을 작성해보세요!'}
